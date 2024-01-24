372917f333682de4424b012280815b8e697012d8
Настя Николаева <anastasia_nikolaeva2002@mail.ru>
2024-01-24 23:50:32 +0300

    добавили код

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Linq;
 
-namespace ik1
+class Program
 {
-    class Program
+    static void Main(string[] args)
+    {  
+        string[] workArray = FillArray();
+        string[] resultArray = GenerateNewArray(workArray);
+        string firstArray = PrintArray(workArray);
+        string secondArray = PrintArray(resultArray);
+        Console.WriteLine(firstArray + " -> " + secondArray);
+    }
+
+    static string[] FillArray()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Введите данные: ");
+        string enterSymbols = Console.ReadLine() ?? string.Empty;
+        char[] separators = { ',', ' ' };
+        return enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    static string PrintArray(string[] workArray)
+    {
+        return "[" + string.Join(", ", workArray.Select(item => $"\"{item}\"")) + "]";
+    }
+
+    static string[] GenerateNewArray(string[] workArray)
+    {
+        return workArray.Where(item => item.Length <= 3).ToArray();
     }
 }
